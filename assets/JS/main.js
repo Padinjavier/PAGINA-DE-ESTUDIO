54ba637dd8058f3f2b0ab6e781210e6efe67ded6
javier <javierpadin661@gmail.com>
2022-03-29 21:20:14 -0500

    accion para cambiar a meet de grupo o clases

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,27 +1,70 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var modeSwitch = document.querySelector('.mode-switch')
+const tipemode = document.querySelector('.mode-switch')
+const html = document.querySelector('html')
 
-  modeSwitch.addEventListener('click', function () {
-    document.documentElement.classList.toggle('dark');
-    modeSwitch.classList.toggle('active')
+const toggleElement = (element, nameClass) => {
+	element.classList.toggle(nameClass)
+}
+
+// mode dack 
+tipemode.addEventListener('click', () => {
+	toggleElement(html, 'dark')
+})
+
+// seccion de meet claces o del grupo
+const card1 = document.querySelector('.meet')
+const menu1 = document.querySelector('.menu1')
+const card2 = document.querySelector('.grupo')
+const menu2 = document.querySelector('.menu2')
+
+card1.addEventListener('click', function () {
+  card1.classList.add('active')
+  card2.classList.remove('active')
+  menu1.classList.add('active')
+  menu2.classList.remove('active')
+})
+
+card2.addEventListener('click', function () {
+  card1.classList.remove('active')
+  card2.classList.add('active')
+  menu1.classList.remove('active')
+  menu2.classList.add('active')
+})
+
+
+// grid o list de meet
+var list1 = document.querySelector('.list1')
+var grid1 = document.querySelector('.grid1')
+var card_info1 = document.querySelector('.card1')
+  
+  list1.addEventListener('click', function () {
+    grid1.classList.remove('active')
+    list1.classList.add('active')
+    card_info1.classList.remove('GRID')
+    card_info1.classList.add('LIST')
   });
   
+  grid1.addEventListener('click', function () {
+    grid1.classList.add('active')
+    list1.classList.remove('active')
+    card_info1.classList.remove('LIST')
+    card_info1.classList.add('GRID')
+  });
+
+  // grid o list de grupo
+  var list2 = document.querySelector('.list2')
+  var grid2 = document.querySelector('.grid2')
+  var card_info2 = document.querySelector('.card2')
   
-  var listView = document.querySelector('.list-view')
-  var gridView = document.querySelector('.grid-view')
-  var projectsList = document.querySelector('.project-boxes')
-  
-  listView.addEventListener('click', function () {
-    gridView.classList.remove('active')
-    listView.classList.add('active')
-    projectsList.classList.remove('GRID')
-    projectsList.classList.add('LIST')
+  list2.addEventListener('click', function () {
+    grid2.classList.remove('active')
+    list2.classList.add('active')
+    card_info2.classList.remove('GRID')
+    card_info2.classList.add('LIST')
   });
   
-  gridView.addEventListener('click', function () {
-    gridView.classList.add('active')
-    listView.classList.remove('active')
-    projectsList.classList.remove('LIST')
-    projectsList.classList.add('GRID')
+  grid2.addEventListener('click', function () {
+    grid2.classList.add('active')
+    list2.classList.remove('active')
+    card_info2.classList.remove('LIST')
+    card_info2.classList.add('GRID')
   });
-});
